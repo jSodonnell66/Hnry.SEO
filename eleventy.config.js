101d8e547dc8ddac9ace71e7e2030d1148547241
Jack O'Donnell <jack.odonnell@developersinstitute.co.nz>
2023-09-05 10:01:49 +1200

    additions made to 11ty config file

--- eleventy.config.js
+++ eleventy.config.js
@@ -2,4 +2,23 @@ const pluginWebc = require("@11ty/eleventy-plugin-webc");
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(pluginWebc);
+
+  eleventyConfig.addPassthroughCopy("static");
+
+  eleventyConfig.setOutputDir("public");
+
+  eleventyConfig.setTemplateFormats(["md", "webc", "html"]);
+
+  eleventyConfig.setLibrary(
+    "md",
+    require("markdown-it")(/* your options here */)
+  );
+
+  // Add other custom configurations or filters as needed
+
+  // Add collections or custom data as needed
+
+  // Add custom shortcodes as needed
+
+  // Return any additional configuration options
 };
