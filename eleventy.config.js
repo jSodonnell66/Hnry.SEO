77a4985e2416bb244a60a25c0dbac20c5955965c
Jack O'Donnell <jodo608@aucklanduni.ac.nz>
2024-03-15 11:45:21 +1300

    webc deletion: project template now njks

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,25 +1,15 @@
-const pluginWebc = require("@11ty/eleventy-plugin-webc");
-const { EleventyRenderPlugin } = require("@11ty/eleventy");
+const nunjucks = require("nunjucks");
 
 module.exports = function (eleventyConfig) {
-    eleventyConfig.addPlugin(EleventyRenderPlugin);
-
-    eleventyConfig.addPlugin(pluginWebc, {
-        components: "_src/_components/*.webc",
-        useTransform: false,
-        transformData: {},
-        bundlePluginOptions: {},
-    });
-
     eleventyConfig.addCollection('post', function(collectionApi) {
         return collectionApi.getAll().filter(item => item.data.layout !== 'landing').sort((a, b) => b.date - a.date);
     });
 
     eleventyConfig.addPassthroughCopy("static");
 
-    eleventyConfig.setTemplateFormats(["md", "webc", "html", "liquid"]);
+    eleventyConfig.setTemplateFormats(["md", "html", "njk"]);
 
-    eleventyConfig.setLibrary("md", require("markdown-it")());
+    eleventyConfig.setLibrary("njk", nunjucks);
 
     return {
         dir: {
