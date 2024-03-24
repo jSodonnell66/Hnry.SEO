890f8832ba699a0cf94f809163e8839686ed4309
Jack O'Donnell <jackodonnell@localhost-live.lan>
2024-03-24 19:44:25 +1300

    file name corrections for js and css dirs

--- eleventy.config.js
+++ eleventy.config.js
@@ -11,7 +11,7 @@ module.exports = function (eleventyConfig) {
         dir: {
             input: "_posts",
             output: "_site",
-            includes: "_includes"
+            includes: "../_includes"
         },
     };      
 };
