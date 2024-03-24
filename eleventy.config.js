b01c67d8b461786153b42e4fa84dff96f24a6d50
Jack O'Donnell <jackodonnell@localhost-live.lan>
2024-03-24 18:56:39 +1300

    reformatting of config file - js and css added to dir mistakenly

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,13 +1,17 @@
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("static");
+
+    eleventyConfig.addPassthroughCopy("_scripts");
+
+    eleventyConfig.addPassthroughCopy("_styles");
+
     eleventyConfig.setTemplateFormats(["md", "html", "njk"]);
 
     return {
         dir: {
             input: "_posts",
             output: "_site",
-            includes: "../_includes",
-             scripts: "../_scripts",
+            includes: "../_includes"
         },
     };
 };
